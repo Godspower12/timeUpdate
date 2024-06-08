f1f1439cd683425ea4b2c62c3be6538e91f81600
Godspower <ogbudhugodspower@gmail.com>
2024-06-08 09:06:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [hours, setHours] = useState('');
+  const [minutes, setMinutes] = useState('');
+  const [progress, setProgress] = useState(null);
+
+  const calculateProgress = (hours, minutes) => {
+      const totalMinutes = (parseInt(hours) * 60) + parseInt(minutes);
+      const totalTimeofTheDay = 24 * 60;
+      const percentage = (totalMinutes / totalTimeofTheDay) * 100;
+      return percentage.toFixed(2);
+  };
+
+  const handleTimeChange = () => {
+      if (hours !== '' && minutes !== '') {
+          const progress = calculateProgress(hours, minutes);
+          setProgress(progress);
+      } else {
+          setProgress(null);
+      }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <label>
+              Time:
+              <input 
+                  type="number" 
+                  value={hours} 
+                  onChange={(e) => setHours(e.target.value)} 
+                  placeholder="h" 
+                  min="0" 
+                  max="23" 
+              />
+              :
+              <input 
+                  type="number" 
+                  value={minutes} 
+                  onChange={(e) => setMinutes(e.target.value)} 
+                  placeholder="MM" 
+                  min="0" 
+                  max="59" 
+              />
+          </label>
+          <button onClick={handleTimeChange} style={{ marginLeft: '10px' }}>Calculate</button>
+          {progress !== null && (
+            <div style={{ marginTop: '10px' }}> {hours}: {minutes} <span style={{marginLeft: '5px'}}>{progress}%</span> </div>
+          )}
+      </div>
   );
-}
+};
+
+
 
 export default App;
